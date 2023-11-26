07d5530618838cfedd5e3c7a239e67ff4e24da55
christian-the-brewer <brewer.christiang@gmail.com>
2023-11-25 19:02:50 -0500

    working on fraction class

---
+++ chapter_02/Fraction/src/Fraction.java
@@ -0,0 +1,20 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int num) {
+        numerator = num;
+    }
+
+    public void setDenominator(int denom) {
+        denominator = denom;
+    }
+}
